563154069d7e5fcae963efe518e0d4f8cbb6a7cf
Michael <michaelcapawana@hotmail.com>
2018-09-20 16:36:21 -0600

    added teach01

---
+++ web/teach01/ta01.js
@@ -0,0 +1,14 @@
+function buttonClicked() {
+    alert("Clicked!")
+	}
+
+/*function changeColor() {
+  var colorInput = "change";
+  var textBox = document.getElementById(colorInput);
+
+  var d_id = "d1";
+  var div = document.getElementById(d_id);
+
+  var color = textBox.value;
+  div.style.background = color;
+  }*/
