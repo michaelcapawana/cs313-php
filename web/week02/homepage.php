4985fc2e9befb4afb5ad66afb34be3f5a78eb864
Michael <michaelcapawana@hotmail.com>
2018-09-21 18:34:01 -0600

    stuff

--- web/week02/homepage.php
+++ web/week02/homepage.php
@@ -3,7 +3,7 @@
 <body>
 
 <h1>Michael Capawana</h1>
-<img src="sandpoint.png" alt="Sandpoint" width="500" height="333">
+<img src="sandpoint.png" alt="Sandpoint" width="500" height="500">
 <?php
 echo "Studies Computer Science at BYU-Idaho";
 echo "Accepted an offer from USAA as a Software Enigineer";
