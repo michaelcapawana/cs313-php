71be9633427c182438d7371a92acbedf574d1648
Michael <michaelcapawana@hotmail.com>
2018-09-21 18:44:52 -0600

    stuff

--- web/week02/homepage.php
+++ web/week02/homepage.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <title>Michael Capawana Homepage</title>
+  <link rel="stylesheet" type="text/css" href="homepage.css" />
+</head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="assign.html">Coming Soon</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-<h1>Michael Capawana</h1>
-<img src="sandpoint.png" alt="Sandpoint" width="500" height="500">
-<?php
-echo "Studies Computer Science at BYU-Idaho";
-echo "Accepted an offer from USAA as a Software Enigineer";
-echo "Married for 2 years to Andreza";
-echo "Served a mission in Vitoria, Brazil";
-?>
+  <h1>Michael Capawana</h1>
+  <img src="sandpoint.png" alt="Sandpoint" width="500" height="500">
+  <?php
+     echo "Studies Computer Science at BYU-Idaho";
+     echo "Accepted an offer from USAA as a Software Enigineer";
+     echo "Married for 2 years to Andreza";
+     echo "Served a mission in Vitoria, Brazil";
+     ?>
 
 </body>
 </html>
