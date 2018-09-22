3a09e30df5c6fac20b6ba3b79c190b5f4dc0afb3
Michael <michaelcapawana@hotmail.com>
2018-09-21 18:32:04 -0600

    stuff

--- web/week02/homepage.php
+++ web/week02/homepage.php
@@ -2,10 +2,13 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
-<img src="sandpoint.png" alt="Sandpoint">
+<h1>Michael Capawana</h1>
+<img src="sandpoint.png" alt="Sandpoint" width="500" height="333">
 <?php
-echo "Hello World!";
+echo "Studies Computer Science at BYU-Idaho";
+echo "Accepted an offer from USAA as a Software Enigineer";
+echo "Married for 2 years to Andreza";
+echo "Served a mission in Vitoria, Brazil";
 ?>
 
 </body>
