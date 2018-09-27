dfaa17d901101827673a877e7e214e71698c5bf1
Michael <michaelcapawana@hotmail.com>
2018-09-27 15:55:36 -0600

    stuff

--- web/teach02/home.php
+++ web/teach02/home.php
@@ -7,23 +7,22 @@
 </head>
 <body>
  <ul id="navbar">
-  <li><a class="active" href="homepage.php">Home</a></li>
+  <li><a class="active" href="home.php">Home</a></li>
   <li><a href="assignments.php">Coming Soon</a></li>
 </ul>
   <h1>Clothing Store</h1>
 <div class="wrap">
   <img id="clothes" src="clothes.png" alt="clothes">
   <ul id="list">
-    <li>Studies Computer Science at BYU-Idaho</li>
-    <li>Accepted offer from USAA as a Software Engineer</li>
-    <li>Married for 2 years to Andreza Capawana</li>
-    <li>Served a mission in Vitoria, Brazil</li>
+    <li>Shirts</li>
+    <li>Pants</li>
+    <li>Shoes</li>
+    <li>Jewlery</li>
   </ul>
 </div>
 
 <div class="echo">
 <?php
-     echo "Today is " . date("l") . ", " . date("m-d-Y");
       ?>
 </div>
 
