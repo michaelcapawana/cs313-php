b9eb361e511c2d8a0a98ed57521974711174b1db
Michael <michaelcapawana@hotmail.com>
2018-09-26 22:39:45 -0600

    going above and beyond

--- web/week02/homepage.php
+++ web/week02/homepage.php
@@ -6,16 +6,14 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script>
     $(document).ready(function(){
-      $('#sandpoint').fadeIn(0).delay(5000).fadeOut(1500);
-      $('#us').delay(5000).fadeIn(1500);
-<!--      $(".wrap").click(function(){
+      $(".wrap").click(function(){
         $("#sandpoint").fadeOut(500, function() {
           $(this).attr("src", "us.PNG");
           $(this).load(function () {
             $("#us").fadeIn(500);
           });
         });
-      });-->
+      });
     });
   </script>
 </head>
