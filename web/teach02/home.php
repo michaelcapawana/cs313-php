02435066a1507bedf45e6483aa89fb5fc6278268
Michael <michaelcapawana@hotmail.com>
2018-09-27 15:50:45 -0600

    stuff

---
+++ web/teach02/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Michael Capawana Homepage</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+ <ul id="navbar">
+  <li><a class="active" href="homepage.php">Home</a></li>
+  <li><a href="assignments.php">Coming Soon</a></li>
+</ul>
+  <h1>Clothing Store</h1>
+<div class="wrap">
+  <img id="clothes" src="clothes.png" alt="clothes">
+  <ul id="list">
+    <li>Studies Computer Science at BYU-Idaho</li>
+    <li>Accepted offer from USAA as a Software Engineer</li>
+    <li>Married for 2 years to Andreza Capawana</li>
+    <li>Served a mission in Vitoria, Brazil</li>
+  </ul>
+</div>
+
+<div class="echo">
+<?php
+     echo "Today is " . date("l") . ", " . date("m-d-Y");
+      ?>
+</div>
+
+</body>
+</html>
+
