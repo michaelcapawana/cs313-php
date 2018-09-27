fc953dbfde90ad3c94714726b796788361e114e3
Michael <michaelcapawana@hotmail.com>
2018-09-27 16:04:42 -0600

    stuff

---
+++ web/teach02/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Clothing Store</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+ <ul id="navbar">
+  <li><a class="active" href="home.php">Home</a></li>
+  <li><a href="about.php">About Us</a></li>
+  <li><a href="login.php">Login</a></li>
+</ul>
+<div class="wrap">
+  <ul id="list">
+  <li><a class="active" href="">Login as Administrator</a></li>
+  <li><a href="">Login as Tester</a></li>
+  </ul>
+</div>
+
+<div class="echo">
+<?php
+      ?>
+</div>
+
+</body>
+</html>
