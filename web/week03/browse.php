13c65acfc68c2f6ed6acf9ed69f69ab04a9bfcac
Michael <michaelcapawana@hotmail.com>
2018-09-29 14:09:20 -0600

    Adding initial files

---
+++ web/week03/browse.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Michael's Padaria</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+<?php
+//require("nav.php");
+?>
+
+  <h1>Michael's Padaria</h1>
+<div class="wrap">
+  <ul id="list">
+    <li>Coxinha</li>
+    <li>Empadinha de Frango</li>
+    <li>Pao de Mel</li>
+    <li>Salgado</li>
+    <li>Sonho</li>
+    <li>Bolo</li>
+  </ul>
+</div>
+
+</body>
+</html>
+
+
