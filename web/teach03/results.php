c9ee233d761f8b28bbdbf25111b830ff92f18aa4
Michael <michaelcapawana@hotmail.com>
2018-10-02 16:47:54 -0600

    stuff

--- web/teach03/results.php
+++ web/teach03/results.php
@@ -6,11 +6,11 @@ Your email address is: <?php echo $_POST["email"]; ?><br>
 Your major is: <?php echo $_POST["major"]; ?><br>
 Your comments say: <?php echo $_POST["comments"]; ?><br>
 The continents you have visited are: 
-
+<?php
 $continent = $_POST["continents"];
 foreach ($continents as $continent) {
   echo $_POST["continent"]; ?><br>
 }
-
+?>
 </body>
 </html> 
