a43bc71fc0f87e9d62e7ef94c1a739613b64114b
Michael <michaelcapawana@hotmail.com>
2018-10-02 16:22:18 -0600

    stuff

---
+++ web/teach03/results.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your major is:<?php echo $_POST["major"]; ?><br>
+Your comments say: <?php echo $_POST["commments"]; ?><br>
+
+</body>
+</html> 
