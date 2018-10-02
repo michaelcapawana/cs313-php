ac0cf3fd234382fbd50fdc1ff903718bb058de4f
Michael <michaelcapawana@hotmail.com>
2018-10-02 16:34:49 -0600

    stuff

--- web/teach03/results.php
+++ web/teach03/results.php
@@ -3,8 +3,8 @@
 
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
-Your major is:<?php echo $_POST["major"]; ?><br>
-Your comments say: <?php echo $_POST["commments"]; ?><br>
-The continents you have visited are: <?php echo $_POST["continents"]; ?><br>
+Your major is: <?php echo $_POST["major"]; ?><br>
+Your comments say: <?php echo $_POST["comments"]; ?><br>
+The continents you have visited are: <?php echo $_POST["continent"]; ?><br>
 </body>
 </html> 
