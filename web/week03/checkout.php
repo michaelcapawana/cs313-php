ab1782da41df209047dbcde9c33feab47e485047
Michael <michaelcapawana@hotmail.com>
2018-10-03 18:49:00 -0600

    stuff

--- web/week03/checkout.php
+++ web/week03/checkout.php
@@ -1,3 +1,26 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Checkout</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<h1>Checkout</h1>
+<form action="confirmation.php" method="post">
+  Name:<br>
+  <input type="text" name="name">
+  <br>
+  Street Address:<br>
+  <input type="text" name="address">
+  <br>
+  City:<br>
+  <input type="text" name="city">
+  <br>
+  State:<br>
+  <input type="text" name="state">
+  <br>
+  Zip Code:<br>
+  <input type="text" name="zip">
+  <br>
+</form>
+</body>
+</html>
