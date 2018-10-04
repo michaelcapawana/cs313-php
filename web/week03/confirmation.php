34f4bc50c0c6cef1419ace023bc300954a083a95
Michael <michaelcapawana@hotmail.com>
2018-10-03 23:39:25 -0600

    stuff

--- web/week03/confirmation.php
+++ web/week03/confirmation.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 Thank you <?php echo $_POST["name"]; ?> for you purchase<br>
-Your address is: <?php echo $_POST["address"]; ?><br>
-<?php echo $_POST["city"]; ?> , <?php echo $_POST["state"]; ?> <?php echo $_POST["zip"]; ?><br>
+Your order has been sent to: <?php echo $_POST["address"]; ?><br>
+                             <?php echo $_POST["city"]; ?> , <?php echo $_POST["state"]; ?> <?php echo $_POST["zip"]; ?><br>
 
 </body>
 </html>
--- web/week03/confirmation.php~
+++ web/week03/confirmation.php~
@@ -1,3 +1,8 @@
-<?php
+<html>
+<body>
+Thank you <?php echo $_POST["name"]; ?> for you purchase<br>
+Your address is: <?php echo $_POST["address"]; ?><br>
+<?php echo $_POST["city"]; ?> , <?php echo $_POST["state"]; ?> <?php echo $_POST["zip"]; ?><br>
 
-?>
+</body>
+</html>
