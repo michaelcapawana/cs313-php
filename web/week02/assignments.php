9399652213a769fd43398a11c18d1fd9e065da01
Michael <michaelcapawana@hotmail.com>
2018-10-05 19:05:39 -0600

    stuff

--- web/week02/assignments.php
+++ web/week02/assignments.php
@@ -10,7 +10,6 @@
   <li><a href="assignments.php">Assignments</a></li>
 </ul>
 <div class="center">
-<h2>Michael Capawana's Assignments</h1>
   <ul>
     <a class="assignments" href="/week03/browse.php">Week 03 - Shopping Cart</a>
   </ul>
