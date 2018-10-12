4acc9da7b9a0b7d35787042ae0eac5d3be3a122d
Michael <michaelcapawana@hotmail.com>
2018-10-12 17:10:17 -0600

    Inserted Test Data

--- db/myDb.sql
+++ db/myDb.sql
@@ -19,3 +19,16 @@ CREATE TABLE business
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL
 );
+
+INSERT INTO users (username, password) VALUES ('mcapawana', 'password123');
+INSERT INTO users (username, password) VALUES ('acapawana', '123password');
+
+INSERT INTO business (name) VALUES ('Chanchos');
+INSERT INTO business (name) VALUES ('Ohana Hut');
+INSERT INTO business (name) VALUES ('Doughlicious');
+INSERT INTO business (name) VALUES ('ParadICE');
+INSERT INTO business (name) VALUES ('Treeline');
+INSERT INTO business (name) VALUES ('Stick Out');
+
+INSERT INTO reviews (rating, description, user_id, business) VALUES ('5', 'Perfect and Cold!', 1, 4);
+INSERT INTO reviews (rating, description, user_id, business) VALUES ('5', 'Great Mexican Food!', 2, 1);
